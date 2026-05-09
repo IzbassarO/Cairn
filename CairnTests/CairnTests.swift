32f9d99334eb51c2e92388c6cee7ddbd505d2d48
Izbassar <izok2004@gmail.com>
2026-05-10 00:20:07 +0500

    Initial Commit

---
+++ CairnTests/CairnTests.swift
@@ -0,0 +1,17 @@
+//
+//  CairnTests.swift
+//  CairnTests
+//
+//  Created by Izbassar Orynbassar on 10.05.2026.
+//
+
+import Testing
+@testable import Cairn
+
+struct CairnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
